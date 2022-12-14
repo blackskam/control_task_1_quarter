c6520dbd6ebd5d25808ad1aab85d7a90ceca15f2
Skam <_skam_@mail.ru>
2022-12-14 09:53:56 +0500

    add program text

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string data = ReadString("Введите данные через запятую:");
+string[] stringData = data.Split(new char[] { ',' });
+string[] stringRezult = StringFilter(stringData);
+Console.WriteLine("Ваши данные с длиной строки меньше 4 символов:");
+WriteArray(stringRezult);
+
+string[] StringFilter(string[] strlst)
+{
+    string k = "";
+    for (int i = 0; i < strlst.Length; i++)
+    {
+        if (strlst[i].Length < 4)
+        {
+            if (i == strlst.Length - 1)
+            {
+                k += strlst[i];
+            }
+            else
+            {
+                k += strlst[i] + ",";
+            }
+        }
+    }
+    string[] stringRezult = k.Split(new char[] { ',' });
+    return stringRezult;
+}
+
+
+string ReadString(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+
+void WriteArray(string[] strlst)
+{
+    for (int i = 0; i < strlst.Length; i++)
+    {
+        {
+            if (i == strlst.Length - 1)
+            {
+                Console.Write(strlst[i]);
+            }
+            else
+            {
+                Console.Write(strlst[i] + ",");
+            }
+        }
+    }
+}
